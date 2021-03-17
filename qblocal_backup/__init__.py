f54f2bd2dfd22b26ce493aed5d026871be6e0dfa
Cologler <skyoflw@gmail.com>
2021-03-17 19:47:22 +0800

    update for new version

--- qblocal_backup/__init__.py
+++ qblocal_backup/__init__.py
@@ -19,9 +19,20 @@ def backup_qbittorrent(config_dir: str, destzip_path: str,
     - `last_backupzip_path` - the last backuped zip file to compare if provided.
     '''
 
-    BT_backup = os.path.join(config_dir, 'data', 'qBittorrent', 'BT_backup')
-    if not os.path.isdir(BT_backup):
-        logger.error(f'{BT_backup} is not a dir.')
+    def find_BT_backup():
+        BT_backup_dirs = [
+            os.path.join(config_dir, 'qBittorrent', 'BT_backup'), # for new version
+            os.path.join(config_dir, 'data', 'qBittorrent', 'BT_backup'), # for old version
+        ]
+        for backup_dir in BT_backup_dirs:
+            if os.path.isdir(backup_dir):
+                return backup_dir
+        logger.error(f'none of {BT_backup_dirs!r} is a dir.')
+
+    BT_backup = os.path.join(config_dir, 'qBittorrent', 'BT_backup')
+
+    BT_backup = find_BT_backup()
+    if BT_backup is None:
         return
 
     files_to_backup: Dict[str, str] = {}
